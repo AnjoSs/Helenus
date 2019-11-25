6e8199a0ed5a1d446205e692da2d4f73ce04479b
frederike.ramin <frederike.ramin@student.hpi.de>
2019-11-25 09:33:58 +0100

    Implement second part of algorithm

--- DFA2Markov.py
+++ DFA2Markov.py
@@ -1,6 +1,6 @@
 # Find all input sequences of length depth that lead to the target state.
 # Save them in paths.
-def dfs(depth, v, path, target, paths):
+def dfs(depth, v, path, target, paths, G, matrix):
     if depth == 0 and v == target:  # stop clause for successful branch
         flat_path = [item for sublist in path for item in sublist]
         if flat_path not in paths:
@@ -11,7 +11,7 @@ def dfs(depth, v, path, target, paths):
     for u in Q:
         if v in G[u]:  # each vertex u such that (v,u) is an edge
             path.append(matrix[v][u])  # add the edge to current vertex to the path
-            dfs(depth-1, u, path, target, paths)  # recursively check all paths for of shorter depth
+            dfs(depth-1, u, path, target, paths, G, matrix)  # recursively check all paths for of shorter depth
             path.pop()  # remove last element
 
 
@@ -23,45 +23,65 @@ def delta(start_state, input_char):
                 return endState
 
 # algorithm from paper
-def main():
-    Q0 = Q
+def main(Q, F, A, m, matrix):
     G = {}
+    Q0 = Q
     for q in Q:
         # find predecessors of q
         G[q] = []
         for k, value in matrix.items():
             if value[q] != [""]:
                 G[q].append(k)
-    # print(G)
 
     D = {}
     for q in Q:
         # find strings that lead to q of length m
         paths = []
-        dfs(m, q, [], q, paths)
+        dfs(m, q, [], q, paths, G, matrix)
         D[q] = paths
 
-    '''
     for q in Q0:
         while len(D[q]) > 1:
             a = D[q][0]
-            qa = "Q"+D[q][0]
+            qa = "Q" + str(D[q][0])
+            # add qa to matrix
+            qa_matrix_row = {}
+            for state in Q:
+                qa_matrix_row[state] = []
+                matrix[state][qa] = []
+            qa_matrix_row[qa] = []
+            matrix[qa] = qa_matrix_row
             Q.append(qa)
             if q in F:
                 F.append(qa)
             D[qa] = [a]
+            # Dm1[qa] = [a[-1]]
             G[qa] = []
-            # for b in A:
-                # TODO
+            for b in A:
+                matrix[qa][delta(q, b)].append(b)
+                G[delta(q, b)].append(qa)
+            # for delta the the power of -(m-1)
+            Dm1 = {}
+            for y in Q:
+                # find strings that lead to y of length m - 1
+                paths = []
+                dfs(m - 1, y, [], y, paths, G, matrix)
+                Dm1[y] = paths
             for p in G[q]:
-                if delta(p, a[len(a) - 1]) == q:  # TODO second condition
+                if delta(p, a[-1]) == q and a[:-1] in Dm1[p]:
                     G[qa] = p
-                    matrix[p][qa].append(a[len(a) - 1])
+                    matrix[p][qa].append(a[-1])
+                    matrix[p][q].remove(a[-1])
+                    G[q].remove(p)
             for p in G[q]:
                 if all(q != delta(p, x) for x in A):
                     G[q].remove(q)
             D[q].remove(a)
-    '''
+    print(Q)
+    print(D)
+    print(G)
+    print(matrix)
+    # TODO: sadly, not correct yet
 
 
 # Test if algorithm returns the correct paths
@@ -95,5 +115,5 @@ matrix = {"B": {"B": ["b"], "A": ["a"], "B2": [""]},
           "B2": {"B": [""], "A": ["a"], "B2": ["b"]}}
 m = 2
 
-test_dfs()
-main()
+# test_dfs()
+main(Q, F, A, m, matrix)
