bb08c198a5817d5f39fd8f470367af7539313268
frederike.ramin <frederike.ramin@student.hpi.de>
2019-12-05 17:48:44 +0100

    Rename variables

--- DFA2Markov.py
+++ DFA2Markov.py
@@ -1,106 +1,102 @@
 # Find all input sequences of length depth that lead to the target state.
 # Save them in paths.
-def dfs(depth, v, path, target, paths, G, matrix):
-    if depth == 0 and v == target:  # stop clause for successful branch
+def dfs(depth, current_state, path, target, paths, predecessor_dict, matrix):
+    if depth == 0 and current_state == target:  # stop clause for successful branch
         flat_path = [item for sublist in path for item in sublist]
         if flat_path not in paths:
             paths.append(flat_path)
         return
     if depth == 0:  # stop clause for non successful branch
         return
-    for u in Q:
-        if v in G[u]:  # each vertex u such that (v,u) is an edge
-            path.append(matrix[v][u])  # add the edge to current vertex to the path
-            dfs(depth-1, u, path, target, paths, G, matrix)  # recursively check all paths for of shorter depth
+    for u in states:
+        if current_state in predecessor_dict[u]:  # each vertex u such that (v,u) is an edge
+            path.append(matrix[current_state][u])  # add the edge to current vertex to the path
+            dfs(depth - 1, u, path, target, paths, predecessor_dict, matrix)  # recursively check all paths for of shorter depth
             path.pop()  # remove last element
 
 
 # Returns the resulting state when input_char is received in state start_state
 def delta(start_state, input_char, matrix):
-    for endState, wordList in matrix[start_state].items():
-        for word in wordList:
+    for end_state, word_list in matrix[start_state].items():
+        for word in word_list:
             if word != "" and input_char == word[-1]:
-                return endState
+                return end_state
 
 
-def updateD(D, G, m, matrix, Q):
-    for q in Q:
-        # find strings that lead to q of length m
+def update_pre_paths_dict(pre_paths_dict, predecessor_dict, order, matrix, states):
+    for q in states:
+        # find strings that lead to q of length order
         paths = []
-        for t in Q:
-            dfs(m, t, [], q, paths, G, matrix)
-        D[q] = paths
+        for t in states:
+            dfs(order, t, [], q, paths, predecessor_dict, matrix)
+        pre_paths_dict[q] = paths
 
 
 # algorithm from paper
-def main(Q, F, A, m, matrix):
-    G = {}
-    Q0 = list(Q)
-    for q in Q:
+def main(states, final_states, alphabet, order, matrix):
+    predecessor_dict = {}
+    initial_states = list(states)
+    for q in states:
         # find predecessors of q
-        G[q] = []
-        for k, value in matrix.items():
-            if value[q] != [""]:
-                G[q].append(k)
-    D = {}
-    updateD(D, G, m, matrix, Q)
+        predecessor_dict[q] = []
+        for state, transitions in matrix.items():
+            if transitions[q] != [""]:
+                predecessor_dict[q].append(state)
+    pre_paths_dict = {}
+    update_pre_paths_dict(pre_paths_dict, predecessor_dict, order, matrix, states)
 
-    for q in Q0:
-        while len(D[q]) > 1:
-            a = D[q][1]
-            qa = "Q" + str(D[q][1])
+    for q in initial_states:
+        while len(pre_paths_dict[q]) > 1:
+            a = pre_paths_dict[q][1]
+            qa = "Q" + str(pre_paths_dict[q][1])
 
             # add qa to matrix
             qa_matrix_row = {}
-            for state in Q:
+            for state in states:
                 qa_matrix_row[state] = []
                 matrix[state][qa] = []
             qa_matrix_row[qa] = []
             matrix[qa] = qa_matrix_row
-            Q.append(qa)
-            if q in F:
-                F.append(qa)
-            D[qa] = [a]
-            G[qa] = []
+            states.append(qa)
+            if q in final_states:
+                final_states.append(qa)
+            pre_paths_dict[qa] = [a]
+            predecessor_dict[qa] = []
 
-            for b in A:
+            for b in alphabet:
                 matrix[qa][delta(q, b, matrix)].append(b)
-                G[delta(q, b, matrix)].append(qa)
-                # TODO: Validate - do we need that?
-                updateD(D, G, m, matrix, Q)
+                predecessor_dict[delta(q, b, matrix)].append(qa)
 
             # for delta to the power of -(m-1)
             Dm1 = {}
-            for y in Q:
-                # find strings that lead to y of length m - 1
-                paths = []
-                dfs(m - 1, y, [], y, paths, G, matrix)
-                Dm1[y] = paths
+            update_pre_paths_dict(Dm1, predecessor_dict, order - 1, matrix, states)
 
-            for p in G[q]:
+            for p in predecessor_dict[q]:
                 if delta(p, a[-1], matrix) == q and a[:-1] in Dm1[p]:
-                    G[qa].append(p)
+                    predecessor_dict[qa].append(p)
                     matrix[p][qa].append(a[-1])
                     matrix[p][q].remove(a[-1])
-                    G[q].remove(p)
+                    predecessor_dict[q].remove(p)
 
-                    updateD(Dm1, G, m-1, matrix, Q)
-            for p in G[q]:
-                if all(q != delta(p, x, matrix) for x in A):
-                    G[q].remove(q)
-            D[q].remove(a)
-    print(Q)
-    print(D)  # TODO: D[Qab] empty - problem for higher orders?
-    print(G)
-    print(F)
+                    update_pre_paths_dict(Dm1, predecessor_dict, order - 1, matrix, states)
+            for p in predecessor_dict[q]:
+                if all(q != delta(p, x, matrix) for x in alphabet):
+                    predecessor_dict[q].remove(q)
+            pre_paths_dict[q].remove(a)
+    print(states)
+    print(pre_paths_dict)  # TODO: pre_paths_dict[Qab] empty - problem for higher orders?
+    print(predecessor_dict)
+    print(final_states)
     print(matrix)
+
+    assert(matrix == {'B': {'B': ['b'], 'A': ['a'], 'B2': [''], "Q['a', 'a']": [], "Q['b', 'b']": []}, 'A': {'B': [''], 'A': [], 'B2': ['b'], "Q['a', 'a']": ['a'], "Q['b', 'b']": []}, 'B2': {'B': [''], 'A': ['a'], 'B2': [], "Q['a', 'a']": [], "Q['b', 'b']": ['b']}, "Q['a', 'a']": {'B': [], 'A': [], 'B2': ['b'], "Q['a', 'a']": ['a'], "Q['b', 'b']": []}, "Q['b', 'b']": {'B': [], 'A': ['a'], 'B2': [], "Q['a', 'a']": [], "Q['b', 'b']": ['b']}})
 
 
 # Test if algorithm returns the correct paths
 def test_dfs():
     test_matrix = {"B": {"B": ["b"], "A": ["a"], "B2": [""]},
-                      "A": {"B": [""], "A": ["a"], "B2": ["b"]},
-                      "B2": {"B": [""], "A": ["a"], "B2": ["b"]}}
+                   "A": {"B": [""], "A": ["a"], "B2": ["b"]},
+                   "B2": {"B": [""], "A": ["a"], "B2": ["b"]}}
     test_Q = {"B", "A", "B2"}
     G = {}
     for q in test_Q:
@@ -131,13 +127,13 @@ def test_dfs():
     assert sorted(paths) == sorted([["a", "b", "b"], ["b", "b", "b"], ["a", "a", "b"], ["b", "a", "b"]])
 
 
-Q = ["B", "A", "B2"]
-F = ["A", "B2"]
-A = {"a", "b"}
+states = ["B", "A", "B2"]
+final_states = ["A", "B2"]
+alphabet = {"a", "b"}
 initial_matrix = {"B": {"B": ["b"], "A": ["a"], "B2": [""]},
                   "A": {"B": [""], "A": ["a"], "B2": ["b"]},
                   "B2": {"B": [""], "A": ["a"], "B2": ["b"]}}
-m = 2
+order = 2
 
 test_dfs()
-main(Q, F, A, m, initial_matrix)
+main(states, final_states, alphabet, order, initial_matrix)
