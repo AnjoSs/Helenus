f8c3855d98fca960b6b3e92244cfc37b48336c55
frederike.ramin <frederike.ramin@student.hpi.de>
2020-02-17 15:21:56 +0100

    Add spread_quality plots

--- data_plotting.py
+++ data_plotting.py
@@ -3,26 +3,44 @@ import csv
 import matplotlib.pyplot as plt
 
 
-def parse_pred_file(file_path):
+def parse_pred_file(file_path, max_dist):
     with open(file_path) as pred_file:
         r = csv.reader(pred_file, delimiter=';')
         count_1 = 0
         count_0 = 0
         spread = 0
         count_total = 0
-        for row in r:
-            pred = row[3]
-            if pred == '-1':
+        summands = []
+        for row_idx, row in enumerate(r):
+            pred = int(row[3])
+            if pred == -1:
                 count_1 += 1
-            elif pred == '0':
+            elif pred == 0:
                 count_0 += 1
             else:
-                spread = max(spread, int(pred))
+                with open("data/mate.csv") as actual:
+                    r = csv.reader(actual)
+                    for i in range(0, row_idx):
+                        next(r)
+                    current_event = next(r)
+                    assert(row[1] == current_event[0])
+                    actual_spread = -1
+                    counter = 0
+                    for i in range(0, pred):
+                        next_event = next(r)
+                        counter += 1
+                        if next_event[0] == 's':
+                            actual_spread = counter
+                            break
+                    if actual_spread != -1:
+                        summands.append(1-(pred-actual_spread)/max_dist)
             count_total += 1
+        assert(len(summands) == count_total)
+        spread_quality = sum(summands)/count_total
         # print("-1 percentage: " + str(count_1 / count_total) +
         #       "\n0 percentage: " + str(count_0 / count_total) +
         #       "\nmax spread: " + str(max_spread))
-        return (count_1 + count_0) / count_total, spread
+        return (count_1 + count_0) / count_total, spread_quality
 
 
 # parse_pred_file('predictions/mate-2-10-0.7.csv')
@@ -32,74 +50,74 @@ max_distances = [5, 10, 40]
 orders = [1, 2]
 
 """ 02-11 """
-res_1_05 = [0.552, 0.612, 0.678, 0.75, 0.778, 0.778, 0.778, 0.778, 0.778]
-res_1_10 = [0.552, 0.612, 0.678, 0.726, 0.792, 0.702, 0.65, 0.65, 0.65]
-res_1_40 = [0.552, 0.612, 0.678, 0.726, 0.792, 0.834, 0.872, 0.922, 0.966]
-
-res_2_05 = [0.602, 0.646, 0.688, 0.712, 0.760, 0.784, 0.784, 0.784, 0.784]
-res_2_10 = [0.602, 0.642, 0.686, 0.732, 0.740, 0.676, 0.652, 0.656, 0.656]
-res_2_40 = [0.602, 0.642, 0.686, 0.732, 0.786, 0.840, 0.884, 0.916, 0.964]
-
-results = dict()
-""" Order 1 """
-results[5] = res_1_05
-results[10] = res_1_10
-results[40] = res_1_40
-non_trivial_percentages = []
-for i, d in enumerate(max_distances):
-    non_trivial_percentages.append([])
-    for t in thresholds:
-        trivial, max_spread = parse_pred_file('../analysis_data/predictions-02-11-2055/mate-1-' + str(d) + '-' + str(t) + '.csv')
-        non_trivial_percentages[i].append(1 - trivial)
-
-plt.plot(thresholds, results[5], "-b", label='precision for max_dist = 5')
-plt.plot(thresholds, non_trivial_percentages[0], '--b', label='non_trivial_perc for max_dist = 5')
-plt.plot(thresholds, results[5], ".b")
-plt.plot(thresholds, non_trivial_percentages[0], ".b")
-
-plt.plot(thresholds, results[10], "-g", label='precision for max_dist = 10')
-plt.plot(thresholds, non_trivial_percentages[1], '--g', label='non_trivial_perc for max_dist = 10')
-plt.plot(thresholds, results[10], "xg")
-plt.plot(thresholds, non_trivial_percentages[1], "xg")
-
-plt.plot(thresholds, results[40], "-r", label='precision for max_dist = 40')
-plt.plot(thresholds, non_trivial_percentages[2], '--r', label='non_trivial_perc for max_dist = 40')
-plt.plot(thresholds, results[40], ".r")
-plt.plot(thresholds, non_trivial_percentages[2], ".r")
-
-plt.legend()
-plt.title("Order 1 - old")
-plt.show()
-
-""" Order 2 """
-results[5] = res_2_05
-results[10] = res_2_10
-results[40] = res_2_40
-non_trivial_percentages = []
-for i, d in enumerate(max_distances):
-    non_trivial_percentages.append([])
-    for t in thresholds:
-        trivial, max_spread = parse_pred_file('../analysis_data/predictions-02-11-2055/mate-2-' + str(d) + '-' + str(t) + '.csv')
-        non_trivial_percentages[i].append(1 - trivial)
-
-plt.plot(thresholds, results[5], "-b", label='precision for max_dist = 5')
-plt.plot(thresholds, non_trivial_percentages[0], '--b', label='non_trivial_perc for max_dist = 5')
-plt.plot(thresholds, results[5], ".b")
-plt.plot(thresholds, non_trivial_percentages[0], ".b")
-
-plt.plot(thresholds, results[10], "-g", label='precision for max_dist = 10')
-plt.plot(thresholds, non_trivial_percentages[1], '--g', label='non_trivial_perc for max_dist = 10')
-plt.plot(thresholds, results[10], "xg")
-plt.plot(thresholds, non_trivial_percentages[1], "xg")
-
-plt.plot(thresholds, results[40], "-r", label='precision for max_dist = 40')
-plt.plot(thresholds, non_trivial_percentages[2], '--r', label='non_trivial_perc for max_dist = 40')
-plt.plot(thresholds, results[40], ".r")
-plt.plot(thresholds, non_trivial_percentages[2], ".r")
-
-plt.legend()
-plt.title("Order 2 - old")
-plt.show()
+# res_1_05 = [0.552, 0.612, 0.678, 0.75, 0.778, 0.778, 0.778, 0.778, 0.778]
+# res_1_10 = [0.552, 0.612, 0.678, 0.726, 0.792, 0.702, 0.65, 0.65, 0.65]
+# res_1_40 = [0.552, 0.612, 0.678, 0.726, 0.792, 0.834, 0.872, 0.922, 0.966]
+#
+# res_2_05 = [0.602, 0.646, 0.688, 0.712, 0.760, 0.784, 0.784, 0.784, 0.784]
+# res_2_10 = [0.602, 0.642, 0.686, 0.732, 0.740, 0.676, 0.652, 0.656, 0.656]
+# res_2_40 = [0.602, 0.642, 0.686, 0.732, 0.786, 0.840, 0.884, 0.916, 0.964]
+#
+# results = dict()
+# """ Order 1 """
+# results[5] = res_1_05
+# results[10] = res_1_10
+# results[40] = res_1_40
+# non_trivial_percentages = []
+# for i, d in enumerate(max_distances):
+#     non_trivial_percentages.append([])
+#     for t in thresholds:
+#         trivial, spread_quality = parse_pred_file('../analysis_data/predictions-02-11-2055/mate-1-' + str(d) + '-' + str(t) + '.csv', d)
+#         non_trivial_percentages[i].append(1 - trivial)
+#
+# plt.plot(thresholds, results[5], "-b", label='precision for max_dist = 5')
+# plt.plot(thresholds, non_trivial_percentages[0], '--b', label='non_trivial_perc for max_dist = 5')
+# plt.plot(thresholds, results[5], ".b")
+# plt.plot(thresholds, non_trivial_percentages[0], ".b")
+#
+# plt.plot(thresholds, results[10], "-g", label='precision for max_dist = 10')
+# plt.plot(thresholds, non_trivial_percentages[1], '--g', label='non_trivial_perc for max_dist = 10')
+# plt.plot(thresholds, results[10], "xg")
+# plt.plot(thresholds, non_trivial_percentages[1], "xg")
+#
+# plt.plot(thresholds, results[40], "-r", label='precision for max_dist = 40')
+# plt.plot(thresholds, non_trivial_percentages[2], '--r', label='non_trivial_perc for max_dist = 40')
+# plt.plot(thresholds, results[40], ".r")
+# plt.plot(thresholds, non_trivial_percentages[2], ".r")
+#
+# plt.legend()
+# plt.title("Order 1 - old")
+# plt.show()
+#
+# """ Order 2 """
+# results[5] = res_2_05
+# results[10] = res_2_10
+# results[40] = res_2_40
+# non_trivial_percentages = []
+# for i, d in enumerate(max_distances):
+#     non_trivial_percentages.append([])
+#     for t in thresholds:
+#         trivial, max_spread = parse_pred_file('../analysis_data/predictions-02-11-2055/mate-2-' + str(d) + '-' + str(t) + '.csv', d)
+#         non_trivial_percentages[i].append(1 - trivial)
+#
+# plt.plot(thresholds, results[5], "-b", label='precision for max_dist = 5')
+# plt.plot(thresholds, non_trivial_percentages[0], '--b', label='non_trivial_perc for max_dist = 5')
+# plt.plot(thresholds, results[5], ".b")
+# plt.plot(thresholds, non_trivial_percentages[0], ".b")
+#
+# plt.plot(thresholds, results[10], "-g", label='precision for max_dist = 10')
+# plt.plot(thresholds, non_trivial_percentages[1], '--g', label='non_trivial_perc for max_dist = 10')
+# plt.plot(thresholds, results[10], "xg")
+# plt.plot(thresholds, non_trivial_percentages[1], "xg")
+#
+# plt.plot(thresholds, results[40], "-r", label='precision for max_dist = 40')
+# plt.plot(thresholds, non_trivial_percentages[2], '--r', label='non_trivial_perc for max_dist = 40')
+# plt.plot(thresholds, results[40], ".r")
+# plt.plot(thresholds, non_trivial_percentages[2], ".r")
+#
+# plt.legend()
+# plt.title("Order 2 - old")
+# plt.show()
 
 """ Order 1 Graph """
 # plt.plot(thresholds, res_1_05, label='max_dist: 05')
@@ -168,7 +186,7 @@ non_trivial_percentages = []
 for i, d in enumerate(max_distances):
     non_trivial_percentages.append([])
     for t in thresholds:
-        trivial, max_spread = parse_pred_file('predictions/mate-1-' + str(d) + '-' + str(t) + '.csv')
+        trivial, max_spread = parse_pred_file('predictions/mate-1-' + str(d) + '-' + str(t) + '.csv', d)
         non_trivial_percentages[i].append(1 - trivial)
 
 plt.plot(thresholds, results[5], "-", color="#F6A800", label='precision for max_dist = 5')
@@ -204,7 +222,7 @@ non_trivial_percentages = []
 for i, d in enumerate(max_distances):
     non_trivial_percentages.append([])
     for t in thresholds:
-        trivial, max_spread = parse_pred_file('predictions/mate-2-' + str(d) + '-' + str(t) + '.csv')
+        trivial, max_spread = parse_pred_file('predictions/mate-2-' + str(d) + '-' + str(t) + '.csv', d)
         non_trivial_percentages[i].append(1 - trivial)
 
 plt.plot(thresholds, results[5], "-", color="#F6A800", label='precision for max_dist = 5')
